5e4a5053e798648be1bca63b1ad60779ba68aa24
Alex Zarate <azarate45@gmail.com>
2026-05-12 22:32:38 -0500

    Add Objects

---
+++ Assets/Scripts/WeaponSystem/Grenade.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float lifeTime = 2f;
+    //public GameObject impactEffect;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    /*void OnTriggerEnter(Collider col)
+    {
+        //if (impactEffect != null)
+        //{
+        //    Instantiate(
+        //        impactEffect,
+        //        col.contacts[0].point,
+        //        Quaternion.identity
+        //    );
+        //}
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }*/
+}
