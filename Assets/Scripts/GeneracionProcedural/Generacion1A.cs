77bfa7c3ad3778cb94677636575d054aa2c8451e
Hecateo <juniorcubamontero83@gmail.com>
2026-05-18 13:49:10 -0500

    generacion procedural
    se puede generar la parte de arriba

--- Assets/Scripts/GeneracionProcedural/Generacion1A.cs
+++ Assets/Scripts/GeneracionProcedural/Generacion1A.cs
@@ -2,10 +2,27 @@ using UnityEngine;
 
 public class Generacion1A : MonoBehaviour
 {
+    int Eleccion;
+    public GameObject[] salasArriba;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Eleccion = Random.Range(0, salasArriba.Length);
+
+        if(Eleccion == 0)
+        {
+            Instantiate(salasArriba[Eleccion], new Vector3(0.01f, 0f, -0.04f), Quaternion.identity);
+        }
+        else if(Eleccion == 1)
+        {
+            Instantiate(salasArriba[Eleccion], new Vector3(0.01f, 0f, -0.05f), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(salasArriba[Eleccion], new Vector3(0f, 0f, -0.04f), Quaternion.identity);
+        }
+
+
     }
 
     // Update is called once per frame
